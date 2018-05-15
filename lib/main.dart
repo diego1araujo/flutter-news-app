7d7c0c917089d54bfb604810b6dd40744e8e3068
Diego Araujo <diegoishigh@gmail.com>
2018-05-15 16:41:56 -0300

    Create main class

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './views/articles/home.dart';
+
+void main() => runApp(new NewsApp());
+
+class NewsApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new NewsAppState();
+  }
+}
