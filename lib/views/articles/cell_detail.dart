cbdbb805c698bf52d98f462ff4c4e60bddefe61d
Diego Araujo <diegoishigh@gmail.com>
2018-05-15 16:43:55 -0300

    Create cell class for detail internal screen

---
+++ lib/views/articles/cell_detail.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ArticleDetailCell extends StatelessWidget {
+  final article;
+
+  ArticleDetailCell(this.article);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Image.network(article['urlToImage']),
+              new Container(height: 8.0),
+              new Text(
+                article['title'],
+                style: new TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              new Container(height: 8.0),
+              new Text(
+                article['description'],
+                style: new TextStyle(
+                  fontSize: 12.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+        new Divider(),
+      ],
+    );
+  }
+}
