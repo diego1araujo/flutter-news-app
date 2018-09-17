fdff50086b01499f97751f2b82c78931c74f56f3
DIEGO Araujo <diegoishigh@gmail.com>
2018-09-17 16:52:53 -0300

    Add refresh icon button and remove 'new' keyword

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,19 +1,17 @@
+import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 import 'main.dart';
 import 'HomeCell.dart';
 import 'Detail.dart';
 
 class NewsAppState extends State<NewsApp>
 {
-    var articles;
+    List articles = [];
 
-    fetchData() async {
-        final apiKey = 'ac9ed44be725499e8d20dd80d113750f';
-        final url = 'https://newsapi.org/v2/everything?sources=cbs-news&apiKey=$apiKey';
+    Future<List> fetchData() async {
+        final url = 'https://newsapi.org/v2/everything?sources=cbs-news&apiKey=ac9ed44be725499e8d20dd80d113750f';
 
         final response = await http.get(url);
 
@@ -21,20 +19,20 @@ class NewsAppState extends State<NewsApp>
             final map = json.decode(response.body);
 
             setState(() {
-                this.articles = map['articles'];
+                articles = map['articles'];
             });
         }
     }
 
     createFlatButton(context, article) {
-        return new FlatButton(
-            padding: new EdgeInsets.all(0.0),
-            child: new HomeCell(article),
+        return FlatButton(
+            padding: EdgeInsets.all(0.0),
+            child: HomeCell(article),
             onPressed: () {
                 Navigator.push(
                     context,
-                    new MaterialPageRoute(
-                        builder: (context) => new Detail(article),
+                    MaterialPageRoute(
+                        builder: (context) => Detail(article),
                     ),
                 );
             },
@@ -42,25 +40,37 @@ class NewsAppState extends State<NewsApp>
     }
 
     createListView() {
-        return new Center(
-            child: new ListView.builder(
-                itemCount: this.articles != null ? this.articles.length : 0,
-                itemBuilder: (context, i) {
-                    return createFlatButton(context, this.articles[i]);
+        return Center(
+            child: articles == null
+                ? CircularProgressIndicator()
+                : ListView.builder(
+                itemCount: articles != null ? articles.length : 0,
+                itemBuilder: (context, index) {
+                    return createFlatButton(context, articles[index]);
                 },
             ),
         );
     }
 
     @override
-    Widget build(BuildContext context) {
+    void initState() {
+        super.initState();
         fetchData();
+    }
 
-        return new MaterialApp(
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
             debugShowCheckedModeBanner: false,
-            home: new Scaffold(
-                appBar: new AppBar(
-                    title: new Text("News App"),
+            home: Scaffold(
+                appBar: AppBar(
+                    title: Text("News App"),
+                    actions: <Widget>[
+                        IconButton(
+                            icon: Icon(Icons.refresh),
+                            onPressed: fetchData,
+                        ),
+                    ],
                 ),
                 body: createListView(),
             ),
