38627f1bf5a58d285e7452c53087474c5f2690fb
Diego Araujo <diegoishigh@gmail.com>
2019-01-15 14:47:24 -0300

    Add Splash Screen

--- android/app/src/main/java/com/example/newsapprenew/MainActivity.java
+++ android/app/src/main/java/com/example/newsapprenew/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.newsapprenew;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
